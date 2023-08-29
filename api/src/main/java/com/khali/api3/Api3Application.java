4285f2949a6fb47b307732b20957372638e5663d
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-08-29 15:10:59 -0300

    chore: initial setup

---
+++ api/src/main/java/com/khali/api3/Api3Application.java
@@ -0,0 +1,13 @@
+package com.khali.api3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Api3Application.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/khali/api3/Api3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khali.api3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
