9aa59923ce42ee6a2c6017a04803d28da8842e7a
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-08-30 22:15:41 -0300

    add: AppointmentStatus

---
+++ api/src/main/java/com/khali/api3/domain/appointment/AppointmentStatus.java
@@ -0,0 +1,27 @@
+package com.khali.api3.domain.appointment;
+
+public enum AppointmentStatus {
+    Pending(0),
+    Approved(1),
+    Rejected(2);
+
+    public static final AppointmentStatus[] STATUSES = values();
+
+    private int index;
+
+    // Create a new Status from a string. Exclusively for the list above
+    private AppointmentStatus (int index) {
+        this.index = index;
+    }
+
+    // returns the string value of the Status
+    public int getIntValue() {
+        return index;
+    }
+
+    // returns the appointmentStatus corresponding to the given index
+    public static AppointmentStatus of (int index) {
+        if (index >= 0 && index < STATUSES.length) { return STATUSES[index]; }
+        throw new IllegalArgumentException("AppointmentStatus.of -- Error: Invalid argument: " + index);
+    }
+}
