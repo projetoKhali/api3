11cd6537ad232b77115a522c1cda076e676f81dd
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-08-30 21:52:16 -0300

    add: UserType

---
+++ api/src/main/java/com/khali/api3/domain/user/UserType.java
@@ -0,0 +1,26 @@
+package com.khali.api3.domain.user;
+
+public enum UserType {
+    Employer(0),
+    Manager(1),
+    Admin(2);
+
+    public static final UserType[] USER_TYPES = values();
+
+    private int level;
+
+    private UserType (int level) {
+        this.level = level;
+    }
+
+    public static UserType of (int level) {
+        for (UserType userType : USER_TYPES) {
+            if (userType.getLevel() != level) continue;
+            return userType;
+        }
+        throw new IllegalArgumentException("UserType.of -- Error: Invalid argument: " + level);
+    }
+
+    public int getLevel() { return level; }
+}
+
