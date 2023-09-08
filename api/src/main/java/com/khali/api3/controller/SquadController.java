99d9d247e30f353d4bd203a7f6017550632c30b9
Incivius <111438883+Incivius@users.noreply.github.com>
2023-09-08 17:43:32 -0300

    feat: Criação do endpoint /squad

---
+++ api/src/main/java/com/khali/api3/controller/SquadController.java
@@ -0,0 +1,16 @@
+package com.khali.api3.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// import com.khali.api3.service.SquadService;
+@RestController
+@RequestMapping("/squad")
+public class SquadController {
+
+//     @GetMapping
+//     public List<Squad> getAllSquads() {
+//         return squadService.getAllSquads();
+//     }
+
+}
