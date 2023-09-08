4e7d82b3b0deb1b2d4442d2ebee54789ca9a09b4
taniacruzz <tanicruz112@gmail.com>
2023-09-08 16:31:05 -0300

    conexão com BD e criação de tabelas

--- api/src/main/java/com/khali/api3/repositories/AppointmentRepository.java
+++ api/src/main/java/com/khali/api3/repositories/AppointmentRepository.java
@@ -1,13 +1,19 @@
 package com.khali.api3.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 import com.khali.api3.domain.appointment.Appointment;
+import com.khali.api3.domain.user.User;
+
 import java.util.Optional;
 
 
 // manipulação das tabelas
+@RepositoryRestResource
 public interface AppointmentRepository extends JpaRepository<Appointment, Long>{
-    Optional<Appointment> findByUserId(int userId);
+    Optional<Appointment> findByUserId(User user);
 
     default void saveAppointment(Appointment apt){
     }
