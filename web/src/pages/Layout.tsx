2b4d7d082937135a1777ebcdab69bbfe311c40c0
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-09-09 09:13:19 -0300

    wip: fix `SideMenu` example props syntax

--- web/src/pages/Layout.tsx
+++ web/src/pages/Layout.tsx
@@ -2,13 +2,23 @@ import { useState, useEffect } from 'react';
 
 import { Outlet } from 'react-router-dom';
 import SideMenu from '../components/SideMenu';
+import { SideMenuItem } from '../components/SideMenu';
 
 export default function Layout(){
-    const [menuItems, setMenuItems] = useState([]);
+    const [menuItems, setMenuItems] = useState<SideMenuItem[]>([]);
 
-    // useEffect({
-    //     setMenuItems()
-    // }, []);
+    useEffect(() => {
+        setMenuItems([
+            {
+                label: "test 1",
+                url: "#1",
+            },
+            {
+                label: "test 2",
+                url: "#2",
+            },
+        ] as SideMenuItem[]);
+    }, []);
 
     return (
         <>
