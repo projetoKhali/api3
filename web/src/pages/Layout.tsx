2096e14b99f616aa803e78873765267bd997af18
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-09-09 12:13:14 -0300

    chore: moved SideMenuItem generation code to `services/Access` from `components/Layout`

--- web/src/pages/Layout.tsx
+++ web/src/pages/Layout.tsx
@@ -3,21 +3,17 @@ import { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import SideMenu from '../components/SideMenu';
 import { SideMenuItem } from '../components/SideMenu';
+import { GetSideMenuItems, UserType } from '../services/Access';
 
 export default function Layout(){
     const [menuItems, setMenuItems] = useState<SideMenuItem[]>([]);
 
     useEffect(() => {
-        setMenuItems([
-            {
-                label: "test 1",
-                url: "#1",
-            },
-            {
-                label: "test 2",
-                url: "#2",
-            },
-        ] as SideMenuItem[]);
+        setMenuItems(
+            // GetSideMenuItems(UserType.Employee)
+            // GetSideMenuItems(UserType.Manager)
+            GetSideMenuItems(UserType.Admin)
+        );
     }, []);
 
     return (
