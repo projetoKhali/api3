197e514c8c5dff7cf844f75aa6b51f12b47697e0
Mikaéla <mikaela.cardoso@fatec.sp.gov.br>
2023-09-10 19:58:15 -0300

    feat: adição de service AppointmentService e funcionalidades (insert, get, getById, update)

---
+++ api/src/main/java/com/khali/api3/services/AppointmentService.java
@@ -0,0 +1,88 @@
+package com.khali.api3.services;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.khali.api3.domain.appointment.Appointment;
+import com.khali.api3.repositories.AppointmentRepository;
+
+@Service
+public class AppointmentService {
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+    public Appointment saveAppointment(Appointment appointment) {
+        return appointmentRepository.save(appointment);
+    }
+    
+    public List<Appointment> getAppointment(){
+        return appointmentRepository.findAll();
+    }
+
+    public Appointment getAppointmentByID(Long id){
+        return appointmentRepository.findById(id).orElse(null);
+    }
+
+    public Appointment updateAppointment(Long id, Appointment newAppointment){
+        Appointment appointmentExists = appointmentRepository.findById(id).orElse(null);
+        
+        if (appointmentExists != null) {
+            if (newAppointment.getApt_updt() != null) {
+                appointmentExists.setApt_updt(newAppointment.getApt_updt());
+            }
+
+            if (newAppointment.getClient() != null) {
+                appointmentExists.setClient(newAppointment.getClient());
+            }
+
+            if (newAppointment.getEndDate() != null) {
+                appointmentExists.setEndDate(newAppointment.getEndDate());
+            }
+
+            if (newAppointment.getFeedback() != null) {
+                appointmentExists.setFeedback(newAppointment.getFeedback());
+            }
+
+            if (newAppointment.getInsertDate() != null) {
+                appointmentExists.setInsertDate(newAppointment.getInsertDate());
+            }
+
+            if (newAppointment.getJustification() != null) {
+                appointmentExists.setJustification(newAppointment.getJustification());
+            }
+
+            if (newAppointment.getProject() != null) {
+                appointmentExists.setProject(newAppointment.getProject());
+            }
+
+            if (newAppointment.getResultCenter() != null) {
+                appointmentExists.setResultCenter(newAppointment.getResultCenter());
+            }
+
+            if (newAppointment.getStartDate() != null) {
+                appointmentExists.setStartDate(newAppointment.getStartDate());
+            }
+
+            if (newAppointment.getStatus() != null) {
+                appointmentExists.setStatus(newAppointment.getStatus());
+            }
+
+            if (newAppointment.getType() != null) {
+                appointmentExists.setType(newAppointment.getType());
+            }
+
+            if (newAppointment.getUser() != null) {
+                appointmentExists.setUser(newAppointment.getUser());
+            }
+
+            return appointmentRepository.save(appointmentExists);
+        } else {
+            throw new NoSuchElementException("Appointment not found with ID: " + id);
+        }
+    }
+    
+}
