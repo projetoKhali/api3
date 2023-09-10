5b24b2e187c91745528c1ab5c0ae329d5bbd8127
Mikaéla <mikaela.cardoso@fatec.sp.gov.br>
2023-09-10 18:37:34 -0300

    feat: funcionalidade update na PayRateRuleService

--- api/src/main/java/com/khali/api3/services/PayRateRuleService.java
+++ api/src/main/java/com/khali/api3/services/PayRateRuleService.java
@@ -1,6 +1,7 @@
 package com.khali.api3.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,13 +18,54 @@ public class PayRateRuleService {
     public PayRateRule savePayRateRule(PayRateRule payRateRule) {
         return payRateRuleRepository.save(payRateRule);
     }
+    
     public List<PayRateRule> getPayRateRule(){
         return payRateRuleRepository.findAll();
     }
+
     public PayRateRule getPayRateRuleByID(Long id){
-        //System.out.println(payRateRuleRepository.findById(payRateRule.getId()));
         return payRateRuleRepository.findById(id).orElse(null);
     }
+
+    public PayRateRule updatePayRateRule(Long id, PayRateRule newPayRateRule){
+        PayRateRule payRateRuleExists = payRateRuleRepository.findById(id).orElse(null);
+        
+        if (payRateRuleExists != null) {
+            if (newPayRateRule.getCode() != null) {
+                payRateRuleExists.setCode(newPayRateRule.getCode());
+            }
+
+            if (newPayRateRule.getAppointmentType() != null) {
+                payRateRuleExists.setAppointmentType(newPayRateRule.getAppointmentType());
+            }
+
+            if (newPayRateRule.getDaysOfWeek() != null) {
+                payRateRuleExists.setDaysOfWeek(newPayRateRule.getDaysOfWeek());
+            }
+
+            if (newPayRateRule.getEndTime() != null) {
+                payRateRuleExists.setEndTime(newPayRateRule.getEndTime());
+            }
+
+            if (newPayRateRule.getStartTime() != null) {
+                payRateRuleExists.setStartTime(newPayRateRule.getStartTime());
+            }
+
+            if (newPayRateRule.getHourDuration() != 0.0) {
+                payRateRuleExists.setHourDuration(newPayRateRule.getHourDuration());
+            }
+
+            if (newPayRateRule.getPayRate() != 0.0) {
+                payRateRuleExists.setPayRate(newPayRateRule.getPayRate());
+            }
+
+            return payRateRuleRepository.save(payRateRuleExists);
+        } else {
+            throw new NoSuchElementException("PayRateRule não encontrada com o ID: " + id);
+        }
+    }
+
+
 
 }
 
