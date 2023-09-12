fbe36d3c6205f3eb22a354be65d6d927d16b6b1e
Mikaéla <mikaela.cardoso@fatec.sp.gov.br>
2023-09-12 10:49:58 -0300

    fix: sintaxe das tabelas e edição de campos e tipos

--- api/sql/ddl/table.sql
+++ api/sql/ddl/table.sql
@@ -15,47 +15,45 @@ CREATE TYPE Apt_status AS ENUM (
 
 create table if not exists pay_rate_rules(
     id serial primary key,
-    code int,
+    code int UNIQUE,
     hour_duration numeric,
     pay_rate numeric,
     appointment_type Apt_type,
-    start_time timestamp,
-    end_time timestamp,
+    start_time time,
+    end_time time
 );
-ALTER TABLE pay_rate_rules COLUMN start_time TYPE time
-
 
 create table if not exists clients(
     id serial primary key,
-    "name" varchar(255),
-    cnpj varchar(255)
+    name varchar(255),
+    cnpj varchar(255) UNIQUE
 );
 
 create table if not exists users(
     id serial primary key,
     registration varchar(255),
-    "name" varchar(255),
+    name varchar(255),
     user_type User_type,
-    email varchar(255),
-    "password" varchar(255),
-    active bool,
-    insert_date timestamp,
-    expire_date timestamp,
+    email varchar(255) UNIQUE,
+    password varchar(255),
+    active bool TRUE,
+    insert_date timestamp CURRENT_TIMESTAMP,
+    expire_date timestamp
 );
-alter table users alter column insert_date set default(CURRENT_TIMESTAMP);
 
 
 create table if not exists result_centers(
     id serial primary key,
-    "name" varchar(255),
-    code varchar(255),
+    name varchar(255),
+    cod_pay_rate_rules integer,
     acronym varchar(255),
     gst_id int,
-    insert_date timestamp,
+    insert_date timestamp CURRENT_TIMESTAMP,
     CONSTRAINT gst_id_fk FOREIGN KEY
     (gst_id) REFERENCES users (id)
+    CONSTRAINT cod_pay_rate_rules_fk foreign key(cod_pay_rate_rules)
+    REFERENCES pay_rate_rules(cod)
 );
-alter table result_centers alter column insert_date set default(CURRENT_TIMESTAMP);
 
 create table if not exists members(
     usr_id int,
@@ -65,23 +63,24 @@ create table if not exists members(
     CONSTRAINT usr_id_fk FOREIGN key
     (usr_id) REFERENCES users (id),
     CONSTRAINT rc_id_fk FOREIGN key
-    (rc_id) REFERENCES resultcenters (id),
+    (rc_id) REFERENCES resultcenters (id)
 );
 
 create table if not exists appointments(
     id serial primary key,
-    "start_date" timestamp,
-    end_date timestamp,
+    start_date date,
+    end_date date,
     usr_id int,
     clt_id int,
     rc_id int,
     project varchar(255),
     appointment_type Apt_type,
     justification varchar(255),
-    "status" Apt_status,
-    insert_date timestamp,
+    status Apt_status,
+    insert_date timestamp CURRENT_TIMESTAMP,
     apt_updt_id int,
     feedback varchar(255),
+    cod_pay_rate_rules integer,
     CONSTRAINT usr_id_fk foreign key
     (usr_id) references users(id),
     CONSTRAINT clt_id_fk foreign key
@@ -89,12 +88,11 @@ create table if not exists appointments(
     CONSTRAINT rc_id_fk foreign key
     (rc_id) references resultcenters(id),
     CONSTRAINT apt_updt_fk foreign key
-    (apt_updt_id) references appointments
+    (apt_updt_id) references appointments,
+    CONSTRAINT cod_pay_rate_rules_fk foreign key
+    (cod_pay_rate_rules) REFERENCES pay_rate_rules(cod)
 );
-alter table appointments alter column insert_date set default(CURRENT_TIMESTAMP);
-drop view vw_appointments;
-alter table appointments alter start_date type date;
-alter table appointments alter end_date type date;
+
 
 
 
