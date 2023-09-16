a71adf07058cc2ffa1e14954cbdb3a3a7d217733
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-09-15 22:07:01 -0300

    chore: removed unused code from `Api3Application`

--- api/src/main/java/com/khali/api3/Api3Application.java
+++ api/src/main/java/com/khali/api3/Api3Application.java
@@ -12,19 +12,6 @@ import com.khali.api3.services.ClientService;
 public class Api3Application {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(Api3Application.class, args);
-		ConfigurableApplicationContext context = SpringApplication.run(Api3Application.class, args);
-
-		// Obtenha uma instância do serviço ClientService
-        ClientService clientService = context.getBean(ClientService.class);
-		
-		Client cliente = new Client();
-        cliente.setCnpj("44.444.444/001-48");
-        cliente.setName("Judite's company3");
-		// clientService.saveClient(cliente);
-		clientService.deleteClientByCnpj("44.444.444/001-45");
-		context.close();
-
-		
+		SpringApplication.run(Api3Application.class, args);
 	}
 }
