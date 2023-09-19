4dd1695df1a83db937a8d861217d9ef93c67ad32
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-09-19 14:28:20 -0300

    wip (front): `getUserSideMenuItems` function

--- web/src/services/Access.ts
+++ web/src/services/Access.ts
@@ -1,64 +1,61 @@
 import { SideMenuItem } from "../components/SideMenu";
-export enum UserType {
-    Employee,
-    Manager,
-    Admin,
+import axios from 'axios';
+
+const API_URL = 'http://127.0.0.1:8080';
+
+const PermissionSideMenuItemMap: Record<string, SideMenuItem[]> = {
+    "": [
+        {
+            label: "Dashboard",
+            url: "#",
+        },
+    ],
+    "Appoint": [
+        {
+            label: "Apontamentos",
+            url: "#",
+        },
+    ],
+    "Validate": [
+        {
+            label: "Aprovações",
+            url: "#"
+        },
+    ],
+    "Register": [
+        {
+            label: "Usuários",
+            url: "/users/",
+        },
+        {
+            label: "Centros de Resultado",
+            url: "/resultcenters/"
+        },
+        {
+            label: "Clientes",
+            url: "#"
+        },
+        {
+            label: "Projetos",
+            url: "#"
+        },
+    ],
+    "Report": [
+        {
+            label: "Relatório",
+            url: "#"
+        },
+    ]
 }
 
-export function GetSideMenuItems (userType: UserType) {
-    if (userType === UserType.Employee) {
-        return [
-            {
-                label: "Apontamentos",
-                url: "#",
-            },
-            {
-                label: "Dashboard",
-                url: "#"
-            },
-        ] as SideMenuItem[];
-    }
-    else if (userType === UserType.Manager) {
-        return [
-            {
-                label: "Aprovações",
-                url: "#",
-            },
-            {
-                label: "Dashboard",
-                url: "#"
-            },
-        ] as SideMenuItem[];
-    }
-    else if (userType === UserType.Admin) {
-        return [
-
-            {
-                label: "Usuários",
-                url: "#",
-            },
-            {
-                label: "Centros de Resultado",
-                url: "#"
-            },
-            {
-                label: "Clientes",
-                url: "#"
-            },
-            {
-                label: "Projetos",
-                url: "#"
-            },
-            {
-                label: "Relatório",
-                url: "#"
-            },
-            {
-                label: "Dashboard",
-                url: "#"
-            },
-        ] as SideMenuItem[];
-    }
-    console.error(`Services.Access.GetSideMenuItems -- Error: Acessos do menu lateral não foram configurados para o UserType \`${userType}\``);
-    return [];
+export async function getUserSideMenuItems (id: number): Promise<SideMenuItem[]> {
+    const response = await axios.get(`${API_URL}/users/${id}/permissions/`, {});
+    const items: SideMenuItem[] = [];
+    response.data.forEach((permission: string) => {
+        const sideMenuItemsOfPermission: SideMenuItem[] | undefined = PermissionSideMenuItemMap[permission];
+        if (sideMenuItemsOfPermission) {
+            items.push(...sideMenuItemsOfPermission);
+        }
+    });
+    return items;
 }
