a7bd9223b7f1abe4241d6a88970a28d88615c297
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-09-19 15:50:09 -0300

    chore: AppointmentController.java criando metodo savAppointment para persistir informações no banco

--- api/src/main/java/com/khali/api3/controllers/AppointmentController.java
+++ api/src/main/java/com/khali/api3/controllers/AppointmentController.java
@@ -1,9 +1,12 @@
 package com.khali.api3.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.khali.api3.domain.appointment.Appointment;
 import com.khali.api3.repositories.AppointmentRepository;
 
 @RestController
@@ -14,5 +17,10 @@ public class AppointmentController {
     @Autowired
     public AppointmentController(AppointmentRepository appointmentRepository){
         this.appointmentRepository = appointmentRepository;
+    }
+
+    @PostMapping
+    public Appointment insertAppointment(@RequestBody Appointment appointment){
+        return appointmentRepository.save(appointment);
     }
 }
