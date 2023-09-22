bc8cf224a347f7fe944f0c2dcb45e7a74addc18a
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-09-22 17:54:23 -0300

    refactor: [AppointmentController] Adicionando metodos no controller

--- api/src/main/java/com/khali/api3/controllers/AppointmentController.java
+++ api/src/main/java/com/khali/api3/controllers/AppointmentController.java
@@ -1,26 +1,80 @@
 package com.khali.api3.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.khali.api3.domain.appointment.Appointment;
+import com.khali.api3.domain.user.User;
 import com.khali.api3.repositories.AppointmentRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @RestController
-@RequestMapping("/Appointments")
+@RequestMapping("/appointments")
 public class AppointmentController {
     private final AppointmentRepository appointmentRepository;
-    
+
     @Autowired
-    public AppointmentController(AppointmentRepository appointmentRepository){
+    public AppointmentController(AppointmentRepository appointmentRepository) {
         this.appointmentRepository = appointmentRepository;
     }
 
+    @GetMapping
+    public List<Appointment> getAllAppointments() {
+        return appointmentRepository.findAll();
+    }
+
+    @GetMapping
+    public List<Appointment> getAppointmentsByUser(User user) {
+        return appointmentRepository.findAppointmentByUser(user.getId());
+    }
+
+    @GetMapping("/{id}")
+    public Appointment getAppointmentById(@PathVariable Long id) {
+        return appointmentRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Appointment not found with id: " + id));
+    }
+
     @PostMapping
-    public Appointment insertAppointment(@RequestBody Appointment appointment){
+    public Appointment createAppointment(@RequestBody Appointment appointment) {
         return appointmentRepository.save(appointment);
+    }
+
+    @PutMapping("/{id}")
+    public Appointment updateAppointment(@PathVariable Long id, @RequestBody Appointment appointmentDetails) {
+        Appointment appointment = appointmentRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Appointment not found with id: " + id));
+
+        // Update the appointment object with the details from the request body
+        appointment.setUser(appointmentDetails.getUser());
+        appointment.setType(appointmentDetails.getType());
+        appointment.setStartDate(appointmentDetails.getStartDate());
+        appointment.setEndDate(appointmentDetails.getEndDate());
+        appointment.setInsertDate(appointmentDetails.getInsertDate());
+        appointment.setResultCenter(appointmentDetails.getResultCenter());
+        appointment.setClient(appointmentDetails.getClient());
+        appointment.setProject(appointmentDetails.getProject());
+        appointment.setJustification(appointmentDetails.getJustification());
+        appointment.setStatus(appointmentDetails.getStatus());
+        appointment.setFeedback(appointmentDetails.getFeedback());
+        appointment.setApt_updt(appointmentDetails.getApt_updt());
+
+        return appointmentRepository.save(appointment);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteAppointment(@PathVariable Long id) {
+        appointmentRepository.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 }
