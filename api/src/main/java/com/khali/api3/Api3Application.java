4accb31cba3318bd75b6b0652a938bd654c8ba84
taniacruzz <tanicruz112@gmail.com>
2023-09-23 19:25:26 -0300

    inclusão de dependencias do spring secutity

--- api/src/main/java/com/khali/api3/Api3Application.java
+++ api/src/main/java/com/khali/api3/Api3Application.java
@@ -13,17 +13,17 @@ public class Api3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Api3Application.class, args);
-		ConfigurableApplicationContext context = SpringApplication.run(Api3Application.class, args);
+		// ConfigurableApplicationContext context = SpringApplication.run(Api3Application.class, args);
 
-		// Obtenha uma instância do serviço ClientService
-        ClientService clientService = context.getBean(ClientService.class);
+		// // Obtenha uma instância do serviço ClientService
+        // ClientService clientService = context.getBean(ClientService.class);
 		
-		Client cliente = new Client();
-        cliente.setCnpj("44.444.444/001-48");
-        cliente.setName("Judite's company3");
-		// clientService.saveClient(cliente);
-		clientService.deleteClientByCnpj("44.444.444/001-45");
-		context.close();
+		// Client cliente = new Client();
+        // cliente.setCnpj("44.444.444/001-48");
+        // cliente.setName("Judite's company3");
+		// // clientService.saveClient(cliente);
+		// clientService.deleteClientByCnpj("44.444.444/001-45");
+		// context.close();
 
 		
 	}
