6ab0ac39348710c086d5061673eb841c4fc795e9
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-09-23 22:35:40 -0300

    feat (clients | front): add `ClientService`

---
+++ web/src/services/ClientService.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { Client } from '../schemas/Client';
+
+const API_URL = 'http://127.0.0.1:8080/clients';
+
+export async function getClients (): Promise<Client[]> {
+    const response = await axios.get(API_URL, {});
+    return await response.data.map((item: any) => ({
+        key: item.id.toString(), 
+        name: item.name? item.name : "N/A",
+        cnpj: item.cnpj? item.cnpj : "N/A",
+        // active: item.active? item.active : "N/A",
+        // insertDate: item.insertDate? item.insertDate : "N/A",
+        // expireDate: item.expireDate? item.expireDate : "N/A",
+    })) as Client[];
+}
+
+export async function postClient(user: Client){
+    return await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(user)
+    }).then(response=> response.json())
+    .then((data)=> console.log(data))
+    .catch(error => console.error(error));
+    
+}
