eed9dc6d2f03aac97075847531b67ff01e797cde
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-09-23 22:38:38 -0300

    feat (clients | front): mapped `/clients` route to `Clients` page in `App`

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import Appointments from "./pages/Appointments";
-import Client from './pages/Client';
+import Clients from './pages/Clients';
 import Home from "./pages/Home";
 import Layout from './pages/Layout';
 import ResultCenter from './pages/ResultCenters';
-import Users from "./pages/Users";
 
 
 function App() {
@@ -14,8 +13,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Layout/>}>
                     <Route index element={<Home />} />
-                    <Route path="client" element={<Client />} />
-                    <Route path="users" element={<Users/>} />
+                    <Route path="clients" element={<Clients />} />
                     <Route path="resultCenters" element={<ResultCenter/>} />
                     <Route path="appointments" element={<Appointments/>} />
                 </Route>
