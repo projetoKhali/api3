618f6dc7ea8b1af355da1d8a816d3c057426a573
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-09-27 17:30:21 -0300

    wip

---
+++ web/src/components/AppointmentTypeDropdown.tsx
@@ -0,0 +1,14 @@
+import DropdownOption from "../schemas/DropdownOption";
+
+
+interface DropdownProps {
+    onSelect: (option: DropdownOption) => void;
+}
+
+export default function AppointmentTypeDropdown ({onSelect}: DropdownProps) {
+
+
+    return (
+
+    );
+}
