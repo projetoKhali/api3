b9d2f8f0916f5a54528a2695d883b69a18cc657d
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-09-29 17:40:35 -0300

    wip (appointments | front): `AppointmentTypeDropdown` component body iteration

--- web/src/components/AppointmentTypeDropdown.tsx
+++ web/src/components/AppointmentTypeDropdown.tsx
@@ -1,14 +1,29 @@
 import DropdownOption from "../schemas/DropdownOption";
+import Dropdown from './Dropdown';
 
 
-interface DropdownProps {
+interface AppointmentTypeDropdownProps {
     onSelect: (option: DropdownOption) => void;
 }
 
-export default function AppointmentTypeDropdown ({onSelect}: DropdownProps) {
+export default function AppointmentTypeDropdown ({onSelect}: AppointmentTypeDropdownProps) {
+    const options = [
+        {
+            displayName: 'Hora Extra',
+            optionName: 'Overtime',
+        },
+        {
+            displayName: 'Sobreaviso',
+            optionName: 'OnNotice',
+        }
+    ] as DropdownOption[];
 
+    function handleSelect(option: DropdownOption) {
+
+        onSelect(option);
+    }
 
     return (
-
+        <Dropdown options={options} onSelect={handleSelect}/>
     );
 }
