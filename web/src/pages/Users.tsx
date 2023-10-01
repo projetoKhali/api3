a7a5900f9f5ccc1e75bd5dc5b23844f65ca2c8d3
Incivius <marcos.santos232@fatec.sp.gov.br>
2023-10-01 19:44:24 -0300

    feat: <pages users> visualiação do status do usuario e botão desativar

--- web/src/pages/Users.tsx
+++ web/src/pages/Users.tsx
@@ -1,10 +1,10 @@
 import { Table } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import { useEffect, useState } from 'react';
-import { getUsers } from '../services/UserService';
-import { UserSchema } from '../schemas/User';
-import '../styles/userTData.css';
 import UserForm from '../components/UserForm';
+import { UserSchema } from '../schemas/User';
+import { getUsers } from '../services/UserService';
+import '../styles/userTData.css';
 
 export default function Users() {
     const [users, setUsers] = useState<UserSchema[]>([]);
@@ -41,10 +41,16 @@ export default function Users() {
           key: 'userType',
         },
         {
+          title: 'Status',
+          dataIndex: 'active',
+          key: 'active',
+        },
+        {
           dataIndex: 'tags',
           key: 'tags',
-          render: (_,data) => (data? <button>Ver</button> : null)
+          render: (_,data) => (data? <button>Desativar</button> : null)
         },
+        
     ];
     return (
         <div>
