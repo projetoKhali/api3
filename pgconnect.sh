b0c6f8e6e49fc11adcd15b7118c543d8acd0375a
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-10-08 15:27:26 -0300

    feat: `pgconnect` docker compatibility

--- pgconnect.sh
+++ pgconnect.sh
@@ -1,2 +1,8 @@
 . .env
+if [ "$DATABASE_HOSTNAME" == "khali_api3_db" ]; then
+    khali_api3_db_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' khali_api3_db)
+    echo "Connecting to container database ip $khali_api3_db_ip with pgcli..."
+    pgcli -h $khali_api3_db_ip -u $DATABASE_USERNAME -d $DATABASE_NAME
+    exit 0
+fi
 pgcli postgresql://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOSTNAME}:${DATABASE_PORT}/${DATABASE_NAME}
