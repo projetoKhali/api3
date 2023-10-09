9039cc59131d99951ac0f04d51219ffa6fdb6103
taniacruzz <tanicruz112@gmail.com>
2023-10-09 00:17:06 -0300

    chore: função que retorna o ultimo parameter do bd

--- api/src/main/java/com/khali/api3/controllers/ParameterController.java
+++ api/src/main/java/com/khali/api3/controllers/ParameterController.java
@@ -1,11 +1,15 @@
 package com.khali.api3.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.khali.api3.domain.client.Client;
 import com.khali.api3.domain.parameter.Parameter;
 import com.khali.api3.repositories.ParametersRepository;
 
@@ -17,6 +21,17 @@ public class ParameterController {
     public ParameterController(@Autowired ParametersRepository parameterRepository){
         this.parameterRepository = parameterRepository;
     }
+
+    @GetMapping
+    public List<Parameter> getAllParameters() {
+        return parameterRepository.findAll();
+    }
+
+    @GetMapping("/last")
+    public Parameter getLastParameters() {
+        return parameterRepository.findLastParameter();
+    }
+
 
     @PostMapping
     public Parameter createParameter(@RequestBody Parameter parameter) {
