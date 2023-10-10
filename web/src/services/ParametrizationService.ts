5eff9bda91955053b184fc47ac25430161236422
taniacruzz <tanicruz112@gmail.com>
2023-10-09 22:43:57 -0300

    feat: services de parametrizations

---
+++ web/src/services/ParametrizationService.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { ParameterSchema } from '../schemas/Parametrization';
+
+const API_URL = 'http://127.0.0.1:8000/parameters';
+
+export async function getParameters (): Promise<ParameterSchema> {
+    const response = await axios.get(API_URL, {});
+    return await response.data.map((item: any) => ({
+        key: item.id.toString(), 
+        nightShiftStart: item.nightShiftStart? item.nightShiftStart : "N/A",
+        nightShiftEnd: item.nightShiftEnd? item.nightShiftEnd : "N/A",
+        closingDayOfMonth: item.closingDayOfMonth? item.closingDayOfMonth : "N/A",
+        // insertDate: item.insertDate? item.insertDate : "N/A",
+    }))[0] as ParameterSchema;
+}
+
+export async function postParameter(parameter: ParameterSchema) {
+    return await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(parameter)
+    }).then(response=> response.json())
+    .then((data)=> console.log(data))
+    .catch(error => console.error(error));
+    
+}
---
+++ web/src/services/PayRateRulesService.ts
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import { PayRateRuleSchema } from '../schemas/PayRateRule';
+
+const API_URL = 'http://127.0.0.1:8000/payRateRules';
+
+export async function getPayRateRules (): Promise<PayRateRuleSchema[]> {
+    const response = await axios.get(API_URL, {});
+    return await response.data.map((item: any) => ({
+        key: item.id.toString(), 
+        code: item.code? item.code : "N/A",
+        type: item.type? item.type : "N/A",
+        shift: item.shift? item.shift : "N/A",
+        weekend: item.weekend? item.weekend : "N/A",
+        minHourCount: item.minHourCount? item.minHourCount : "N/A",
+        hourDuration: item.hourDuration? item.hourDuration : "N/A",
+        percentage: item.percentage? item.percentage : "N/A",
+        overlap: item.overlap? item.overlap : "N/A",
+        expireDate: item.expireDate? item.expireDate : "N/A",
+    })) as PayRateRuleSchema[];
+}
+
+export async function postParameter(parameter: PayRateRuleSchema) {
+    return await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(parameter)
+    }).then(response=> response.json())
+    .then((data)=> console.log(data))
+    .catch(error => console.error(error));
+   
+}
+
+export async function updateParameter(parameter: PayRateRuleSchema) {
+    return await fetch(API_URL, {
+        method: 'PUT',
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(parameter)
+    }).then(response=> response.json())
+    .then((data)=> console.log(data))
+    .catch(error => console.error(error));
+    
+}
