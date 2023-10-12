3cae9f2501ce833f8365c064c92d5e81d9fee40c
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-10-12 17:04:13 -0300

    chore: removed duplicated `sql` files

--- api/src/main/resources/sql/dml/functions.sql
+++
@@ -1,7 +0,0 @@
-create or replace function fc_desabilitar (usr_id int) returns void as $$ begin
-update users
-set expire_date = now(),
-    active = false
-where id = usr_id;
-end;
-$$ language plpgsql
