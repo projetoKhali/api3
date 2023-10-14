48872689aca5d3e6c39ec79a125ba07740bcd3b3
taniacruzz <tanicruz112@gmail.com>
2023-10-14 00:40:12 -0300

    fix: inclusao do Transient

--- api/src/main/java/com/khali/api3/domain/parameter/Parameter.java
+++ api/src/main/java/com/khali/api3/domain/parameter/Parameter.java
@@ -15,6 +15,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity(name="parameters")
 @Table(name="parameters")
@@ -30,16 +31,17 @@ public class Parameter {
     @Column(name="prm_id")
     private Long id;
 
+    @Transient
     @Column (name = "insert_date")
     private Timestamp insert_date;
 
     @Column (name = "closing_day")
-    private Long closing_day;
+    private Long closingDayOfMonth;
 
     @Column (name = "start_night_time")
-    private LocalTime startNightTime;
+    private LocalTime nightShiftStart;
 
     @Column (name = "end_night_time")
-    private LocalTime endNightTime;
+    private LocalTime nightShiftEnd;
     
 }
