9286624b76d64cb2e9756e482e3cfb77bfc1050f
taniacruzz <tanicruz112@gmail.com>
2023-10-14 00:53:31 -0300

    fix: formatação de string para localtime com regex

--- web/src/components/ParametrizationForm.tsx
+++ web/src/components/ParametrizationForm.tsx
@@ -19,13 +19,16 @@ export default function ParametrizationForm ({successCallback, errorCallback}: P
         const intValue = parseInt(inputValue, 10);
         if (!isNaN(intValue)) {
             setClosingDayOfMonth(intValue);
-          } else {
+        } else {
             setClosingDayOfMonth("");
-          }
+        }
     }
 
     function handleSubmit(event: React.ChangeEvent<HTMLFormElement>) {
-        if (!postNightShiftStart || !postNightShiftEnd || !postClosingDayOfMonth) {return errorCallback ();}
+        if (!postNightShiftStart || !postNightShiftEnd || !postClosingDayOfMonth) {
+            console.log("Hello, world!");
+            return errorCallback ();
+        }
         
         event.preventDefault();
         
@@ -51,7 +54,14 @@ export default function ParametrizationForm ({successCallback, errorCallback}: P
         
 }
 
-function formatTime(dateTimeStr: string): string {
-    return `${dateTimeStr}:00}`
-    
+function formatTime(hora: string): string | null {
+    const regex = /^(0?[0-9]|1[0-9]|2[0-3])(:?[0-5][0-9])?$/;
+    if (!regex.test(hora)) {
+        console.error('Formato de hora inválido. Use o formato de 0 a 23.');
+        return null;
+    }
+
+    const horaFormatada = hora.padEnd(2, '0') + ':00';
+    return horaFormatada;
+      
 }
