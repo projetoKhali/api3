6f9ad9ca8d56a4c9ebc6a31ee6df4ba813acadbd
taniacruzz <tanicruz112@gmail.com>
2023-10-15 20:48:03 -0300

    wip: pop up parametrization e form parametrization

--- web/src/components/ParametrizationForm.tsx
+++ web/src/components/ParametrizationForm.tsx
@@ -3,54 +3,25 @@ import { PostParameterSchema } from "../schemas/Parametrization";
 import { postParameter } from "../services/ParametrizationService";
 
 interface ParametrizationFormProps {
-    successCallback: () => void;
-    errorCallback: () => void;
+    handleNightShiftStartChange: () => void;
+    handleNightShiftEndChange: () => void;
+    handlePostClosingDayOfMonthChange: () => void;   
 }
 
-export default function ParametrizationForm ({successCallback, errorCallback}: ParametrizationFormProps) {
-    const [postNightShiftStart, setNightShiftStart] = useState<string>('');
-    const [postNightShiftEnd, setNightShiftEnd] = useState<string>('');
-    const [postClosingDayOfMonth, setClosingDayOfMonth] = useState<number | string>('');
 
-    function handleNightShiftStartChange(event: React.ChangeEvent<HTMLInputElement>){ setNightShiftStart(event.target.value); }
-    function handleNightShiftEndChange(event: React.ChangeEvent<HTMLInputElement>){ setNightShiftEnd(event.target.value); }
-    function handlePostClosingDayOfMonthChange(event: React.ChangeEvent<HTMLInputElement>){ 
-        const inputValue = event.target.value;
-        const intValue = parseInt(inputValue, 10);
-        if (!isNaN(intValue)) {
-            setClosingDayOfMonth(intValue);
-        } else {
-            setClosingDayOfMonth("");
-        }
-    }
 
-    function handleSubmit(event: React.ChangeEvent<HTMLFormElement>) {
-        if (!postNightShiftStart || !postNightShiftEnd || !postClosingDayOfMonth) {
-            console.log("Hello, world!");
-            return errorCallback ();
-        }
-        
-        event.preventDefault();
-        
-        const formattedNightShiftStart = formatTime (postNightShiftStart);
-        const formattedNightShiftEnd = formatTime (postNightShiftEnd);
-        
-        postParameter({
-            nightShiftStart: formattedNightShiftStart,
-            nightShiftEnd: formattedNightShiftEnd,
-            closingDayOfMonth: postClosingDayOfMonth,
-          } as PostParameterSchema)
-          .then(() => successCallback());
-    }
-
-    const [open, setOpen] = useState<boolean>(false);
-
+export default function ParametrizationForm ({
+    handleNightShiftStartChange,
+    handleNightShiftEndChange,
+    handlePostClosingDayOfMonthChange
+}: ParametrizationFormProps) {
+          
     return (
-        <form onSubmit={handleSubmit}>
+        <form >
             <input type="text" placeholder="Inicio do Período Noturno" onChange={handleNightShiftStartChange}/>
             <input type="text" placeholder="Fim do Período Noturno" onChange={handleNightShiftEndChange}/>
             <input type="text" placeholder="Dia de Fechamento" onChange={handlePostClosingDayOfMonthChange}/>
-            <button type="submit">Salvar</button>
+            
             
         </form>
     );
--- web/src/pages/Parametrization.tsx
+++ web/src/pages/Parametrization.tsx
@@ -4,17 +4,18 @@ import { Table } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import { getPayRateRules } from "../services/PayRateRulesService";
 import ParametrizationForm from "../components/ParametrizationForm";
-import Popup, { PopupSchema } from "../components/PopUp";
+import Popup, { PopupSchema } from "../components/PopUpParametrization";
+import EditableTableCell, { EditableTableColumn } from "../components/EditableTableCell";
 
 export default function Parametrization() {
     const [payRateRules, setPayRateRules] = useState<PayRateRuleSchema[]>([]);
-    
+
     const requestPayRateRules = () => {
         getPayRateRules().then(payRateRulesResponse =>
             setPayRateRules(payRateRulesResponse)
         );
     };
-    
+
     const [popupData, setPopupData] = useState<PopupSchema | null>(null);
     const [showPopup, setShowPopup] = useState(false);
 
@@ -26,65 +27,122 @@ export default function Parametrization() {
         setShowPopup(false);
     };
 
+    const stringToBoolean = (value: string): boolean => {
+        if (value.toLowerCase() === 'true') {
+          return true;
+        } else if (value.toLowerCase() === 'false') {
+          return false;
+        } else {
+          throw new Error('Valor inválido. A string deve ser "true" ou "false".');
+        }
+      };
+
+    const [postNightShiftStart, setNightShiftStart] = useState<string>('');
+    const [postNightShiftEnd, setNightShiftEnd] = useState<string>('');
+    const [postClosingDayOfMonth, setClosingDayOfMonth] = useState<number | string>('');
+
+    function handleNightShiftStartChange(event: React.ChangeEvent<HTMLInputElement>){ setNightShiftStart(event.target.value) }
+    function handleNightShiftEndChange(event: React.ChangeEvent<HTMLInputElement>){ setNightShiftEnd(event.target.value)}
+    function handlePostClosingDayOfMonthChange(event: React.ChangeEvent<HTMLInputElement>){ 
+        const inputValue = event.target.value;
+        const intValue = parseInt(inputValue, 10);
+        if (!isNaN(intValue)) {
+            setClosingDayOfMonth(intValue);
+        } else {
+            setClosingDayOfMonth("");
+        }
+        return postClosingDayOfMonth;
+    }
+
+
     const columns: ColumnsType<PayRateRuleSchema> = [
-        {
+        EditableTableColumn({
             title: 'Verba',
-            dataIndex: 'code',
-            key: 'code',
-        },
-        {
+            getValue: (item: PayRateRuleSchema)=>`${item.code}`,
+            setValue: (item: PayRateRuleSchema, value: string)=>item.code=parseInt(value),
+
+        }),
+        EditableTableColumn({
             title: 'Tipo',
-            dataIndex: 'type',
-            key: 'type',
-        },
-        {
+            getValue: (item: PayRateRuleSchema)=>item.appointmentType,
+            setValue: (item: PayRateRuleSchema, value: string)=>item.appointmentType=value,
+        }),
+        EditableTableColumn({
             title: 'Período',
-            dataIndex: 'shift',
-            key: 'shift',
-        },
-        {
-            title: 'Fim de semana',
-            dataIndex: 'weekend',
-            key: 'weekend',
-        },
-        {
+            getValue: (item: PayRateRuleSchema)=>item.shift,
+            setValue: (item: PayRateRuleSchema, value: string)=>item.shift=value,
+        }),
+        // EditableTableColumn({
+        //     title: 'Fim de semana',
+        //     getValue: (item: PauRateRuleSchema)=>item.weekend,
+        //     setValue: (item: PayRateRuleSchema, value: string)=>item.weekend=value,
+        // }),
+        EditableTableColumn({
             title: 'Mínimo de horas',
-            dataIndex: 'minHourCount',
-            key: 'minHourCount',
-        },
-        {
+            getValue: (item: PayRateRuleSchema)=>`${item.minHourCount}`,
+            setValue: (item: PayRateRuleSchema, value: string)=>item.minHourCount=parseFloat(value),
+        }),
+        EditableTableColumn({
             title: 'Duração da hora',
-            dataIndex: 'hourDuration',
-            key: 'hourDuration',
-        },
-        {
+            getValue: (item: PayRateRuleSchema)=>`${item.hourDuration}`,
+            setValue: (item: PayRateRuleSchema, value: string)=>item.hourDuration=parseFloat(value),
+        }),
+        EditableTableColumn({
             title: 'Porcentagem',
-            dataIndex: 'percentage',
-            key: 'percentage',
-        },
-        {
+            getValue: (item: PayRateRuleSchema)=>`${item.payRate}`,
+            setValue: (item: PayRateRuleSchema, value: string)=>item.payRate=parseFloat(value),
+        }),
+        EditableTableColumn({
             title: 'Cumulativo',
-            dataIndex: 'overlap',
-            key: 'overlap',
-        },
+            getValue: (item: PayRateRuleSchema)=>`${item.overlap}`,
+            setValue: (item: PayRateRuleSchema, value: string)=>item.overlap=stringToBoolean(value),          
+        }),
     ]
+
+    function handleSubmit(event: React.ChangeEvent<HTMLFormElement>, ) {
+
+
+        if (!postNightShiftStart || !postNightShiftEnd || !postClosingDayOfMonth) {
+            console.log("Hello, world!");
+            return errorCallback ();
+        }
+        
+        event.preventDefault();
+        
+        const formattedNightShiftStart = formatTime (postNightShiftStart);
+        const formattedNightShiftEnd = formatTime (postNightShiftEnd);
+        
+        postParameter({
+            nightShiftStart: formattedNightShiftStart,
+            nightShiftEnd: formattedNightShiftEnd,
+            closingDayOfMonth: postClosingDayOfMonth,
+          } as PostParameterSchema)
+          .then(() => successCallback());
+    }
     // const [open, setOpen] = useState<boolean>(false);
     return (
         <div>
             <ParametrizationForm
-                errorCallback={() => {}}
-                successCallback={() => {}}
+                errorCallback={() => { }}
+                successCallback={() => { }}
             />
             <button type="button" onClick={() => {
-                    setPopupData({
-                        text: 'Cadastro de Verbas',
-                        buttons: [{ text: 'x', onClick: handleClose }],
-                        isOpen: true,
-                });
-                    setShowPopup(true);}}>
-            Cadastrar Verba
+                handleSubmit();
+            }}>
+                Salvar
             </button>
-            {payRateRules? (
+            <button type="button" onClick={() => {
+                setPopupData({
+                    text: 'Cadastro de Verbas',
+                    buttons: [{ text: 'x', onClick: handleClose }],
+                    isOpen: true,
+                    
+                });
+                setShowPopup(true);
+            }}>
+                Cadastrar Verba
+            </button>
+            {payRateRules ? (
                 <Table dataSource={payRateRules} columns={columns} />
             ) : (
                 null
@@ -92,5 +150,5 @@ export default function Parametrization() {
             {showPopup && popupData && <Popup {...popupData} />}
         </div>
     );
-    
+
 }
