9827833f3e06e899eff41a43cb423bb3125ec03a
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-10-15 00:35:47 -0300

    style: uppercase SQL syntax in `data.sql`

--- api/sql/ddl/data.sql
+++ api/sql/ddl/data.sql
@@ -1,11 +1,11 @@
-insert into users (registration, name, user_type, email, password) values ('123', 'emp', 'Employee'::user_type, 'e@m.p', '1');
-insert into users (registration, name, user_type, email, password) values ('124', 'man', 'Manager'::user_type, 'm@a.n', '1');
-insert into users (registration, name, user_type, email, password) values ('125', 'adm', 'Admin'::user_type, 'a@d.m', '1');
+INSERT INTO users (registration, name, user_type, email, password) VALUES ('123', 'emp', 'Employee'::user_type, 'e@m.p', '1');
+INSERT INTO users (registration, name, user_type, email, password) VALUES ('124', 'man', 'Manager'::user_type, 'm@a.n', '1');
+INSERT INTO users (registration, name, user_type, email, password) VALUES ('125', 'adm', 'Admin'::user_type, 'a@d.m', '1');
 
-insert into result_centers (name, code, acronym, gst_id) values ('result center', 1, 'rc', 2);
+INSERT INTO result_centers (name, code, acronym, gst_id) VALUES ('result center', 1, 'rc', 2);
 
-insert into members (usr_id, rc_id) values (1, 1);
+INSERT INTO members (usr_id, rc_id) VALUES (1, 1);
 
-insert into clients (name, cnpj) values ('client', '1');
+INSERT INTO clients (name, cnpj) VALUES ('client', '1');
 
-insert into projects (name, description) values ('project', 'Khali API3 test project');
+INSERT INTO projects (name, description) VALUES ('project', 'Khali API3 test project');
