dc2a28f284ee91bddfa4316f22c119e34f82fe7c
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-10-15 14:51:49 -0300

    style: removed unnecessary commends and whitespace

--- api/src/main/java/com/khali/api3/repositories/AppointmentRepository.java
+++ api/src/main/java/com/khali/api3/repositories/AppointmentRepository.java
@@ -11,9 +11,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import com.khali.api3.domain.appointment.Appointment;
 import com.khali.api3.domain.resultCenter.ResultCenter;
 
-
-
-// manipulação das tabelas
 @RepositoryRestResource
 public interface AppointmentRepository extends JpaRepository<Appointment, Long>{
     // Optional<Appointment> findByUser(User user);
