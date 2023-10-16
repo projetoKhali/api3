a25d41618a55f6f4df1c29f4b528855c616ca00a
taniacruzz <tanicruz112@gmail.com>
2023-10-15 21:33:51 -0300

    feat: formulário do pop up de cadastro de verbas

--- web/src/components/PopupForm.tsx
+++ web/src/components/PopupForm.tsx
@@ -1,72 +1,81 @@
 import { useEffect, useState } from "react";
 import { PostPayRateRuleSchema } from "../schemas/PayRateRule";
 import { postPayRateRule } from "../services/PayRateRulesService";
+import { Checkbox } from "antd";
 
 interface PopupFormProps {
     successCallback: () => void;
     errorCallback: () => void;
 }
 
-export default function PopupForm ({successCallback, errorCallback}: PopupFormProps) {
+export default function PopupForm({ successCallback, errorCallback }: PopupFormProps) {
     const [postCode, setCode] = useState<number | string>('');
-    const [postType, setType] = useState<string>('');
+    const [postAppointmentType, setAppointmentType] = useState<string>('');
     const [postShift, setShift] = useState<string>('');
-    const [postWeekend, setWeekend] = useState<string>('');
+    // const [postWeekend, setWeekend] = useState<string>('');
     const [postMinHourCount, setMinHourCount] = useState<number | string>('');
     const [postHourDuration, setHourDuration] = useState<number | string>('');
-    const [postPercentage, setPercentage] = useState<number | string>('');
+    const [postPayRate, setPayRate] = useState<number | string>('');
     const [postOverlap, setOverlap] = useState<boolean>(false);
 
 
-    function handleCodeChange(event: React.ChangeEvent<HTMLInputElement>){ setCode(event.target.value); }
-    function handleTypeChange(event: React.ChangeEvent<HTMLInputElement>){ setType(event.target.value); }
-    function handleShiftChange(event: React.ChangeEvent<HTMLInputElement>){ setShift(event.target.value); }
-    function handleWeekendChange(event: React.ChangeEvent<HTMLInputElement>){ setWeekend(event.target.value); }
-    function handleMinHourCountChange(event: React.ChangeEvent<HTMLInputElement>){ setMinHourCount(event.target.value); }
-    function handleHourDurationChange(event: React.ChangeEvent<HTMLInputElement>){ setHourDuration(event.target.value); }
-    function handlePercentageChange(event: React.ChangeEvent<HTMLInputElement>){ setPercentage(event.target.value); }
-    function handleOverlapChange(event: React.ChangeEvent<HTMLInputElement>){ setOverlap(event.target.checked); }
-    
-    
+    function handleCodeChange(event: React.ChangeEvent<HTMLInputElement>) { setCode(event.target.value); }
+    function handleAppointmentTypeChange(event: React.ChangeEvent<HTMLInputElement>) { setAppointmentType(event.target.value); }
+    function handleShiftChange(event: React.ChangeEvent<HTMLInputElement>) { setShift(event.target.value); }
+    // function handleWeekendChange(event: React.ChangeEvent<HTMLInputElement>){ setWeekend(event.target.value); }
+    function handleMinHourCountChange(event: React.ChangeEvent<HTMLInputElement>) { setMinHourCount(event.target.value); }
+    function handleHourDurationChange(event: React.ChangeEvent<HTMLInputElement>) { setHourDuration(event.target.value); }
+    function handlePayRateChange(event: React.ChangeEvent<HTMLInputElement>) { setPayRate(event.target.value); }
+    function handleOverlapChange(event: React.ChangeEvent<HTMLInputElement>) { setOverlap(event.target.checked); }
+
+
     function handleSubmit(event: React.ChangeEvent<HTMLFormElement>) {
-        if (!postCode || !postType || !postShift || !postWeekend || !postMinHourCount 
-            || !postHourDuration || !postPercentage || !postOverlap) {
+        if (!postCode || !postAppointmentType || !postShift || !postMinHourCount
+            || !postHourDuration || !postPayRate || !postOverlap) {
             console.log("Hello, world!");
-            return errorCallback ();
+            // return errorCallback ();
         }
-        
+
+        console.log("Hello, world!2");
         event.preventDefault();
 
         postPayRateRule({
             code: postCode,
-            type: postType,
+            appointmentType: postAppointmentType,
             // shift refere-se a se é diurno ou noturno
             shift: postShift,
-            weekend: postWeekend,
+            // weekend: postWeekend,
             minHourCount: postMinHourCount,
             hourDuration: postHourDuration,
-            percentage: postPercentage,
+            payRate: postPayRate,
             overlap: postOverlap,
-          } as PostPayRateRuleSchema)
-          .then(() => successCallback());
+        } as PostPayRateRuleSchema)
+            .then(() => successCallback());
     }
-
-    const [open, setOpen] = useState<boolean>(false);
 
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Codigo" onChange={handleCodeChange}/>
-            <input type="text" placeholder="Tipo" onChange={handleTypeChange}/>
-            <input type="text" placeholder="Periodo" onChange={handleShiftChange}/>
-            <input type="text" placeholder="Fim de Semana" onChange={handleWeekendChange}/>
-            <input type="text" placeholder="Mínimo de horas" onChange={handleMinHourCountChange}/>
-            <input type="text" placeholder="Duração da hora" onChange={handleHourDurationChange}/>
-            <input type="text" placeholder="Porcentagem" onChange={handlePercentageChange}/>
-            <input type="text" placeholder="Cumulativo" onChange={handleOverlapChange}/>
+            <input type="text" placeholder="Codigo" onChange={handleCodeChange} />
+            <input type="text" placeholder="Tipo" onChange={handleAppointmentTypeChange} />
+            <input type="text" placeholder="Periodo" onChange={handleShiftChange} />
+            {/* <input type="text" placeholder="Fim de Semana" onChange={handleWeekendChange}/> */}
+            <input type="text" placeholder="Mínimo de horas" onChange={handleMinHourCountChange} />
+            <input type="text" placeholder="Duração da hora" onChange={handleHourDurationChange} />
+            <input type="text" placeholder="Porcentagem" onChange={handlePayRateChange} />
+            <p>
+                <Checkbox
+
+                    checked={postOverlap}
+                    onChange={()=>setOverlap(!postOverlap)} />
+                Aceitar Sobreposição
+
+            </p>
+
+
             <button type="submit">Salvar</button>
-            
+
         </form>
     );
-        
+
 }
 
