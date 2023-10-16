9ed224cb412b9e7c75f2c4d7a0e88d6581bacaf3
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-10-15 22:31:41 -0300

    fix: deactivate `Crytography`

--- api/src/main/java/com/khali/api3/domain/user/Cryptography.java
+++ api/src/main/java/com/khali/api3/domain/user/Cryptography.java
@@ -1,18 +1,18 @@
-package com.khali.api3.domain.user;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class Cryptography {
-    private static BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-    // encripita a senha e retorna um hash
-    public static String crypt(String password) {
-        return encoder.encode(password);
-    }
-
-    // verifica se a senha é igual ao hash
-    public static boolean checkPw(String password, String hash) {
-        return encoder.matches(password, hash);
-    }
-
-}
+// package com.khali.api3.domain.user;
+//
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+// public class Cryptography {
+//     private static BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//
+//     // encripita a senha e retorna um hash
+//     public static String crypt(String password) {
+//         return encoder.encode(password);
+//     }
+//
+//     // verifica se a senha é igual ao hash
+//     public static boolean checkPw(String password, String hash) {
+//         return encoder.matches(password, hash);
+//     }
+//
+// }
