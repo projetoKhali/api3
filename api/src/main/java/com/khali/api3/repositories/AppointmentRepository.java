a77a53c1f66a645197e1dbaa31507ec4a176056b
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-10-18 01:39:34 -0300

    feat (apt | back): `updateStatusAppointment` function in `AppointmentRepository`

--- api/src/main/java/com/khali/api3/repositories/AppointmentRepository.java
+++ api/src/main/java/com/khali/api3/repositories/AppointmentRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.khali.api3.domain.appointment.Appointment;
+import com.khali.api3.domain.appointment.AppointmentStatus;
 import com.khali.api3.domain.resultCenter.ResultCenter;
 
 
@@ -25,7 +26,14 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Long>{
     @Query(value = "SELECT * FROM appointments a WHERE a.usr_id = :usr_id", nativeQuery = true)
     List<Appointment> findAppointmentByUser(@Param("usr_id") Long userId);
 
-    @Query(value = "select * from appointments a where rc_id in ( select rc_id from result_centers where gst_id = :usr_id) and status = 'Pending';", nativeQuery = true)
+    @Query(value = "select * from appointments where rc_id in ( select rc_id from result_centers where gst_id = :usr_id) and status = 'Pending';", nativeQuery = true)
     List<Appointment> findByManager(@Param("usr_id") Long userId);
+
+    @Query(value= "update appointments set status = :#{#status.name()} where apt_id = :apt_id returning *;", nativeQuery = true)
+    Optional<Appointment> updateStatusAppointment(
+        @Param("apt_id") Long apt_id,
+        @Param("status") AppointmentStatus status
+    );
+
 }
 
