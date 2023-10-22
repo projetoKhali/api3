d3d219968449d21319d4e98eceb2c3603922dcc1
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-10-22 17:46:44 -0300

    feat: separated `build` and `run` commands in `wdev.bat`

--- api/wdev.bat
+++ api/wdev.bat
@@ -1,6 +1,22 @@
 @echo off
+setlocal
 
+rem Check if the first argument is "build"
+if "%1" == "build" (
+    call :build
+)
+
+rem Check if the first argument is "run"
+if "%1" == "run" (
+    call :run
+)
+
+endlocal
+exit /b 0
+
+:build
 :: Run "mvn clean install" with debug and error output
+echo Building the project...
 mvn clean install -e -X
 
 :: Check the exit code
@@ -8,10 +24,16 @@ if %errorlevel% neq 0 (
     echo Error compiling project
     exit /b 1
 )
+exit /b 0
+
+:run
+:: Load the `.env` file to set the values for `application.properties`
+echo Loading environment variables from .env file...
+for /f "delims=" %%a in (.env) do set %%a
 
 :: Run "mvn spring-boot:run" with debug and error output
+echo Running the Spring Boot application...
 mvn spring-boot:run -e -X
 
 :: Exit the script
 exit /b 0
-
