9ec1fa5e92f7bf6acecfd34f9466fdbced1cbad3
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-10-25 22:50:57 -0300

    chore: bump `AppointmentRepository` version from branch `appointment_controller`

--- api/src/main/java/com/khali/api3/repositories/AppointmentRepository.java
+++ api/src/main/java/com/khali/api3/repositories/AppointmentRepository.java
@@ -12,19 +12,24 @@ import com.khali.api3.domain.appointment.Appointment;
 import com.khali.api3.domain.appointment.AppointmentStatus;
 import com.khali.api3.domain.resultCenter.ResultCenter;
 
+
+
+// manipulação das tabelas
 @RepositoryRestResource
 public interface AppointmentRepository extends JpaRepository<Appointment, Long>{
 
     public List<Appointment> findByResultCenter(ResultCenter resultCenter);
-    public List<Appointment> findAll();
     public Optional<Appointment> findById(Long id);
     
     // utiliza anotações JPA para saber qual tabela e atributo fazer a pesquisa
     @Query(value = "SELECT * FROM appointments a WHERE a.usr_id = :usr_id", nativeQuery = true)
     List<Appointment> findAppointmentByUser(@Param("usr_id") Long userId);
 
-    @Query(value = "select * from appointments where rc_id in ( select rc_id from result_centers where gst_id = :usr_id) and status = 'Pending';", nativeQuery = true)
+    @Query(value = "select * from appointments a where rc_id in ( select rc_id from result_centers where gst_id = :usr_id) and status = 'Pending';", nativeQuery = true)
     List<Appointment> findByManager(@Param("usr_id") Long userId);
+    
+    @Query(value = "select * from appointments a where a.active = true;", nativeQuery = true)
+    public List<Appointment> findByActive();
 
     @Query(value= "update appointments set status = :#{#status.name()} where apt_id = :apt_id returning *;", nativeQuery = true)
     Optional<Appointment> updateStatusAppointment(
