091850d02a99d2981bec9e82b280ae1bcbb9b5ae
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-10-25 22:51:13 -0300

    wip: slice generation

--- api/src/main/java/com/khali/api3/controllers/SliceController.java
+++ api/src/main/java/com/khali/api3/controllers/SliceController.java
@@ -1,6 +1,7 @@
 package com.khali.api3.controllers;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.khali.api3.domain.slice.Slice;
+import com.khali.api3.domain.appointment.Appointment;
 import com.khali.api3.repositories.AppointmentRepository;
 
 @RestController
@@ -32,6 +34,27 @@ public class SliceController {
         Optional<Timestamp> periodEnd
     ) {
         List<Slice> slices = new ArrayList<Slice>();
+
+        appointmentRepository.findByActive()
+            .stream()
+            // .filter() // filter by periodStart & periodEnd
+            .<List<Slice>>map(appointment -> generateSlicesAppointment(appointment))
+            .forEach(generatedSlices -> generatedSlices.forEach(
+                slice -> generatedSlices.add(slice)
+            ));
+
+        return slices;
+    }
+
+    public List<Slice> generateSlicesAppointment (Appointment appointment) {
+        List<Slice> slices = new ArrayList<Slice>();
+
+        // start from endDate
+        // identify day
+        // identify shift 
+        // match PayRateRule
+        // continue recursion
+
         return slices;
     }
 }
