5db20537b28e1ad33de2eb6c09693a25d9e60d7f
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-10-27 20:04:53 -0300

    chore: update `Appoitnment` and `PayRateRule` reference types in `Slice`

--- api/src/main/java/com/khali/api3/domain/slice/Slice.java
+++ api/src/main/java/com/khali/api3/domain/slice/Slice.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import com.khali.api3.domain.appointment.Appointment;
+import com.khali.api3.domain.pay_rate_rule.PayRateRule;;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,14 +17,14 @@ import com.khali.api3.domain.appointment.Appointment;
 @Setter
 @EqualsAndHashCode
 public class Slice {
-    private Long appointmentId;
-    private Long payRateRuleId;
+    private Appointment appointment;
+    private PayRateRule payRateRule;
     private Timestamp start;
     private Timestamp end;
 
     public static Slice fromAppointment (Appointment appointment) {
         return new Slice(
-            appointment.getId(),
+            appointment,
             null,
             appointment.getStartDate(),
             appointment.getEndDate()
