643106f368a77a2224cedaae8306e3aa8f194695
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-10-31 20:35:15 -0300

    chore (apt | back): removed unnecessary syntax sugar in `AppointmentRepository`

--- api/src/main/java/com/khali/api3/repositories/AppointmentRepository.java
+++ api/src/main/java/com/khali/api3/repositories/AppointmentRepository.java
@@ -25,10 +25,10 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Long>{
     @Query(value = "SELECT * FROM appointments a WHERE a.usr_id = :usr_id", nativeQuery = true)
     List<Appointment> findAppointmentByUser(@Param("usr_id") Long userId);
 
-    @Query(value = "select * from appointments a where rc_id in ( select rc_id from result_centers where gst_id = :usr_id) and status = 'Pending';", nativeQuery = true)
+    @Query(value = "select * from appointments where rc_id in ( select rc_id from result_centers where gst_id = :usr_id) and status = 'Pending';", nativeQuery = true)
     List<Appointment> findByManager(@Param("usr_id") Long userId);
     
-    @Query(value = "select * from appointments a where a.active = true;", nativeQuery = true)
+    @Query(value = "select * from appointments where active = true;", nativeQuery = true)
     public List<Appointment> findByActive();
 
     @Query(value= "update appointments set status = :#{#status.name()} where apt_id = :apt_id returning *;", nativeQuery = true)
