873bddfc0629c666982ce17a6d79109e74535458
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-10-31 20:36:40 -0300

    fix (report | back): use `expire_date` instead of `active` in `PayRateRuleRepository` queries

--- api/src/main/java/com/khali/api3/repositories/PayRateRuleRepository.java
+++ api/src/main/java/com/khali/api3/repositories/PayRateRuleRepository.java
@@ -10,15 +10,14 @@ import java.util.Optional;
 
 
 @RepositoryRestResource
-public interface PayRateRuleRepository extends JpaRepository<PayRateRule, Long>{
-    public PayRateRule save(PayRateRule payRateRule);
+public interface PayRateRuleRepository extends JpaRepository<PayRateRule, Long> {
     public List<PayRateRule> findAll();
     public List<PayRateRule> findByCode(Long code);
     public Optional<PayRateRule> findById(Long id);
 
-    @Query(value = "select * from pay_rate_rules where active = true and overlap = true", nativeQuery = true)
+    @Query(value = "select * from pay_rate_rules where expire_date = null and overlap = true", nativeQuery = true)
     public List<PayRateRule> findCumulative ();
 
-    @Query(value = "select * from pay_rate_rules where active = true and min_hour_count > 0", nativeQuery = true)
+    @Query(value = "select * from pay_rate_rules where expire_date = null and min_hour_count > 0", nativeQuery = true)
     public List<PayRateRule> findMinHourCount ();
 }
