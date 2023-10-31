fc0842a40d54ad547cd71af7bdb7a81e9c36f383
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-10-31 01:47:24 -0300

    wip (report | back): wip logic for cumulative `PayRateRule` slice generation in `SliceController`

--- api/src/main/java/com/khali/api3/controllers/SliceController.java
+++ api/src/main/java/com/khali/api3/controllers/SliceController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.khali.api3.domain.util.Pair;
 import com.khali.api3.domain.slice.Slice;
 import com.khali.api3.domain.pay_rate_rule.PayRateRule;
 import com.khali.api3.domain.appointment.Appointment;
@@ -41,17 +42,19 @@ public class SliceController {
         List<PayRateRule> payRateRulesCumulative = payRateRuleRepository.findCumulative();
         List<PayRateRule> payRateRulesMinHourCount = payRateRuleRepository.findMinHourCount();
 
-        appointmentRepository.findByActive()
-            .stream()
-            // .filter() // filter by periodStart & periodEnd
-            .<List<Slice>>map(appointment -> generateSlicesAppointment(
-                Slice.fromAppointment(appointment),
-                payRateRulesCumulative,
-                payRateRulesMinHourCount
-            ))
-            .forEach(generatedSlices -> generatedSlices.forEach(
-                slice -> generatedSlices.add(slice)
-            ));
+        appointmentRepository
+        .findByActive()
+        .stream()
+        // .filter() // filter by periodStart & periodEnd
+        // split days before generateSlicesAppointment
+        .<List<Slice>>map(appointment -> generateSlicesAppointment(
+            Slice.fromAppointment(appointment),
+            payRateRulesCumulative,
+            payRateRulesMinHourCount
+        ))
+        .forEach(generatedSlices -> generatedSlices.forEach(
+            slice -> generatedSlices.add(slice)
+        ));
 
         return slices;
     }
@@ -63,27 +66,90 @@ public class SliceController {
 
     ) {
         List<Slice> subSlices = new ArrayList<Slice>();
+        List<Slice> subSlicesCumulative = new ArrayList<Slice>();
 
-        // no need to diferentiate cumulative rules yet
-        // the first check should be for minHourCount rules
         for (PayRateRule payRateRule: payRateRulesCumulative) {
-            for (Slice subSliceCumulative: getCumulativeSubSlices(appointmentSlice, payRateRule)) {
-                subSlices.addAll(generateSlicesAppointment(subSliceCumulative));
-            }
+            subSlicesCumulative.addAll(getSubSlicesCumulative(appointmentSlice, payRateRule));
         }
+
+
 
 
 
         // start from endDate
         // identify day
-        // identify shift 
+        // identify shift
         // match PayRateRule
         // continue recursion
 
         return subSlices;
     }
 
-    public List<Slice> getSubSlicesCumulative (Slice appointmentSlice, PayRateRule PayRateRule) {
+    public List<Slice> getSubSlicesCumulative (Slice appointmentSlice, PayRateRule payRateRule) {
 
+        // Get the overlap between given slice and payRateRule
+        Optional<Pair<Timestamp>> overlapOptional = getPayRateRuleSliceOverlap(appointmentSlice, payRateRule);
+
+        // no subslices of the given rule
+        if (overlapOptional.isEmpty()) return List.of();
+
+        // overlap is present, get the value inside the optional
+        Pair<Timestamp> overlap = overlapOptional.get();
+
+        // create a new list with the subslice of intersection
+        List<Slice> slices = new ArrayList<>(
+            List.of(
+                Slice.fromPair(
+                    appointmentSlice.getAppointment(),
+                    overlap,
+                    payRateRule
+                )
+            )
+        );
+
+        // get the remaining span of the original slice
+        Optional<Pair<Timestamp>> remainingOptional = getRemainingFromOverlap(
+            appointmentSlice,
+            overlap
+        );
+
+        // if there's remaining 
+        if (remainingOptional.isPresent()) {
+
+            // continue the recursion with the remaining and add the return to the slices list
+            slices.addAll(
+                getSubSlicesCumulative(
+                    Slice.fromPair(
+                        appointmentSlice.getAppointment(),
+                        remainingOptional.get(),
+                        payRateRule
+                    ),
+                    payRateRule
+                )
+            );
+        }
+
+        return slices;
+    }
+
+    public Optional<Pair<Timestamp>> getPayRateRuleSliceOverlap (Slice slice, PayRateRule payRateRule) {
+        // TODO: FIX: payRateRule doesn't have Timestamps to compare
+        // TODO: Also check for shift and daysOfWeek
+        Timestamp start = Math.max(slice.getStart(), payRateRule.getStartDate());
+        Timestamp end = Math.min(slice.getEnd(), payRateRule.getEndDate());
+        return start.before(end) ? Optional.of(new Pair<Timestamp>(start, end)) : Optional.empty();
+    }
+
+    public Optional<Pair<Timestamp>> getRemainingFromOverlap(Slice slice, Pair<Timestamp> overlap) {
+        Timestamp sliceStart = slice.getStart();
+        Timestamp sliceEnd = slice.getEnd();
+        Timestamp overlapStart = overlap.getStart();
+        Timestamp overlapEnd = overlap.getEnd();
+        if (sliceStart.equals(overlapStart) && sliceEnd.equals(overlapEnd)) {
+            return Optional.empty();
+        }
+        return sliceStart.before(overlapStart)
+            ? Optional.of(new Pair<Timestamp>(sliceStart, overlapStart))
+            : Optional.of(new Pair<Timestamp>(overlapEnd, sliceEnd));
     }
 }
