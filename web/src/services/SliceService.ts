a9d66db20e5712b8d7d105b627483ca368a81a0e
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-11-01 01:04:03 -0300

    fix (report | front): expect references instead of ids while mapping `SliceSchema` in `SliceService`

--- web/src/services/SliceService.ts
+++ web/src/services/SliceService.ts
@@ -6,8 +6,8 @@ const API_URL = 'http://127.0.0.1:8080/slices';
 
 function serializeSlice (slice: any): SliceSchema {
     return {
-        appointmentId: slice.appointmentId ? slice.appointmentId : "N/A",
-        payRateRuleId: slice.payRateRuleId ? slice.payRateRuleId : "N/A",
+        appointment: slice.appointment ? slice.appointment : "N/A",
+        payRateRule: slice.payRateRule ? slice.payRateRule : "N/A",
         start: slice.start ? slice.start : "N/A",
         end: slice.end ? slice.end : "N/A",
     }
