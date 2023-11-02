0316dadd02f9289f13921589b36b23e66c2d642c
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-11-02 16:50:32 -0300

    chore: <CsvController.java> retirando dados de teste

--- api/src/main/java/com/khali/api3/controllers/CsvController.java
+++ api/src/main/java/com/khali/api3/controllers/CsvController.java
@@ -43,52 +43,25 @@ public class CsvController {
             if(camposBoolean[8]) header.add("Cliente");
             if(camposBoolean[9]) header.add("Projeto");
             if(camposBoolean[10]) header.add("Justificativa");
-            // header.add("Matricula");
-            // header.add("Colaborador");
-            // header.add("Verba");
-            // header.add("Porcentagem da Verba");
-            // header.add("Hora Início");
-            // header.add("Hora Fim");
-            // header.add("Tipo de Apontamento");
-            // header.add("Centro Resultado");
-            // header.add("Cliente");
-            // header.add("Projeto");
-            // header.add("Justificativa");
 
             csvWriter.writeNext(header.toArray(String[]::new));
             
-            // for (Slice slice : slices) {
-            //     List<String> data = new ArrayList<>();
-            //     if (camposBoolean[0]) data.add(slice.getAppointment().getUser().getRegistration());
-            //     if (camposBoolean[1]) data.add(slice.getAppointment().getUser().getName());
-            //     if (camposBoolean[2]) data.add(slice.getPayRateRule().getCode().toString());
-            //     if (camposBoolean[3]) data.add(slice.getPayRateRule().getPayRate().toString());
-            //     if (camposBoolean[4]) data.add(slice.getStart().toString());
-            //     if (camposBoolean[5]) data.add(slice.getEnd().toString());
-            //     if (camposBoolean[6]) data.add(slice.getAppointment().getType().toString());
-            //     if (camposBoolean[7]) data.add(slice.getAppointment().getResultCenter().getName());
-            //     if (camposBoolean[8]) data.add(slice.getAppointment().getClient().getName());
-            //     if (camposBoolean[9]) data.add(slice.getAppointment().getProject().getName());
-            //     if (camposBoolean[10]) data.add(slice.getAppointment().getJustification());
+            for (Slice slice : slices) {
+                List<String> data = new ArrayList<>();
+                if (camposBoolean[0]) data.add(slice.getAppointment().getUser().getRegistration());
+                if (camposBoolean[1]) data.add(slice.getAppointment().getUser().getName());
+                if (camposBoolean[2]) data.add(slice.getPayRateRule().getCode().toString());
+                if (camposBoolean[3]) data.add(slice.getPayRateRule().getPayRate().toString());
+                if (camposBoolean[4]) data.add(slice.getStart().toString());
+                if (camposBoolean[5]) data.add(slice.getEnd().toString());
+                if (camposBoolean[6]) data.add(slice.getAppointment().getType().toString());
+                if (camposBoolean[7]) data.add(slice.getAppointment().getResultCenter().getName());
+                if (camposBoolean[8]) data.add(slice.getAppointment().getClient().getName());
+                if (camposBoolean[9]) data.add(slice.getAppointment().getProject().getName());
+                if (camposBoolean[10]) data.add(slice.getAppointment().getJustification());
 
-            //     csvWriter.writeNext(data.toArray(new String[0]));
-            // }
-
-            // testando com dados aleatórios
-            List<String> data = new ArrayList<>();
-            if (camposBoolean[0]) data.add("123456");
-            if (camposBoolean[1]) data.add("Fulano de Tal");
-            if (camposBoolean[2]) data.add("123");
-            if (camposBoolean[3]) data.add("100");
-            if (camposBoolean[4]) data.add("2021-09-01 08:00:00");
-            if (camposBoolean[5]) data.add("2021-09-01 12:00:00");
-            if (camposBoolean[6]) data.add("Desenvolvimento");
-            if (camposBoolean[7]) data.add("Centro Resultado");
-            if (camposBoolean[8]) data.add("Cliente");
-            if (camposBoolean[9]) data.add("Projeto");
-            if (camposBoolean[10]) data.add("Justificativa");
-            
-            csvWriter.writeNext(data.toArray(new String[0]));
+                csvWriter.writeNext(data.toArray(new String[0]));
+            }
 
             csvWriter.close();
             System.out.println("Arquivo CSV gerado com sucesso!");
