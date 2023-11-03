c040b01ee925f19b71dd22e2ea367ba7b64ead8a
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-11-03 15:56:28 -0300

    chore (apt | back): removed `active`

--- api/src/main/java/com/khali/api3/repositories/AppointmentRepository.java
+++ api/src/main/java/com/khali/api3/repositories/AppointmentRepository.java
@@ -18,7 +18,7 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Long> 
 
     public List<Appointment> findAll();
 
-    @Query(value = "select * from appointments where active = true", nativeQuery = true)
+    @Query(value = "select * from appointments where apt_updt_id = null", nativeQuery = true)
     public List<Appointment> findByActive();
 
     public List<Appointment> findByResultCenter(ResultCenter resultCenter);
