fd7d24b0c110d500f143a9c6e1ae5ae5794d05ca
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-11-07 20:34:58 -0300

    feat (report | back | dbg): `test_api.sh`

---
+++ test_api.sh
@@ -0,0 +1,2 @@
+curl -sS "http://localhost:8080/slices" | jq -r '.[] | {apt_sta: .appointment.startDate, apt_end: .appointment.endDate, prt_cod: .payRateRule.code, sli_sta: .start, sli_end: .end}' >> response.json
+jq '.' response.json | lolcat
