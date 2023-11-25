c4d8abfca7cbfd6a03c29b195161a13331693fcd
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-11-25 14:34:12 -0300

    chore: update `test_api`

--- test_api.sh
+++ test_api.sh
@@ -1,3 +1,2 @@
-echo "{ \"\": \"$(date)\" }" >> response.json
-curl -sS "http://localhost:8080/slices" | jq -r '.[] | {apt_sta: .appointment.startDate, apt_end: .appointment.endDate, prt_cod: .payRateRule.code, sli_sta: .start, sli_end: .end}' >> response.json
-jq '.' response.json | lolcat
+echo "{ \"\": \"$(date)\" }"
+curl -sS "http://localhost:8080/slices" | jq -r '.[] | {apt_sta: .appointment.startDate, apt_end: .appointment.endDate, prt_cod: .payRateRule.code, prt_mhc: .payRateRule.minHourCount, sli_sta: .start, sli_end: .end}'
