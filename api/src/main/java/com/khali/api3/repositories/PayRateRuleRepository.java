cd0089f9ad22b7229666933087e3720e2aa70977
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-11-25 19:35:11 -0300

    chore: remove unused code and imports

--- api/src/main/java/com/khali/api3/repositories/PayRateRuleRepository.java
+++ api/src/main/java/com/khali/api3/repositories/PayRateRuleRepository.java
@@ -2,7 +2,6 @@ package com.khali.api3.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.khali.api3.domain.pay_rate_rule.PayRateRule;
